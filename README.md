baaebc3d227dca727d3d243ebdac43fb7c83dc00
amaurybrisou <amaury.brisou@gmail.com>
2014-07-31 17:42:08 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Puzzledge.eu 
---
+++ index.js
@@ -0,0 +1,27 @@
+exports.register = function (plugin, options, next) {
+  var path = process.env.puzzledge_blog = __dirname
+
+  var t = new require(path + '/controllers/getTwitter');
+  new t(options);
+
+  plugin.views({
+    engines: {
+      jade: require('jade')
+    },
+    path: path + '/views/'
+  })
+
+  plugin.bind({
+    config: options
+  })
+
+  plugin.route(require('./routes'))
+
+  next()
+}
+
+exports.register.attributes = {
+  name: 'blog.puzzledge.eu',
+  version: '0.0.1',
+  // pkg: require('./package.json')
+}
